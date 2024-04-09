e3d9d1de9e34d1e35ce2bb446372b2ad1d24dc6c
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-09 21:10:24 +0300

    getting information from the frontend

--- projects/mongoDB_webSockets/index.js
+++ projects/mongoDB_webSockets/index.js
@@ -1,26 +1,33 @@
 import dotenv from "dotenv";
-
-import express from "express";
+import { WebSocketServer } from "ws";
 import mongoose from "mongoose";
 
 dotenv.config();
-const app = express();
+const wss = new WebSocketServer({ port: process.env.PORT });
 
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, (err) => {
-      if (err) {
-        console.log("Error", err);
-      }
-      console.log("Server running on port", process.env.PORT);
+    wss.on("connection", (ws) => {
+      console.log("Client connected");
+      ws.on("message", (message) => {
+        message = JSON.parse(message);
+        switch (message.type) {
+          case "registration":
+            console.log("Registration:", message);
+            break;
+          case "login":
+            console.log("Login:", message);
+            break;
+        }
+        // console.log("Received:", message);
+      });
+      ws.on("close", () => {
+        console.log("Client disconnected");
+      });
     });
   })
   .catch((err) => {
     console.log("DB error", err);
   });
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
